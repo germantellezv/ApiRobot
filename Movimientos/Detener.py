dab535bcea0c4bcc8df19f596c450f3b36a2b9d0
Germán Téllez Vanegas <36169542+gertellezv@users.noreply.github.com>
2018-10-18 03:45:34 -0500

    Update Detener.py

--- Movimientos/Detener.py
+++ Movimientos/Detener.py
@@ -2,12 +2,15 @@ import RPi.GPIO as GPIO
 import os
 import time
 
+# GPIO pin defintions
 ena = 18
 in1 = 23
 in2 = 24
 enb = 19
 in3 = 6
 in4 = 5
+
+# Configure GPIO pins
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(ena,GPIO.OUT)
@@ -18,17 +21,33 @@ GPIO.setup(enb,GPIO.OUT)
 GPIO.setup(in3,GPIO.OUT)
 GPIO.setup(in4,GPIO.OUT)
 
-pwm_a = GPIO.PWM(ena,500) # 500Hz es la frecuencia del motor
-pwm_b = GPIO.PWM(enb,500) #
+pwm_a = GPIO.PWM(ena,500)  
+pwm_b = GPIO.PWM(enb,500) 
 
-pwm_a.start(0)   # Se inicia el motor con un 0% de esfuerzo o Duty Cycle
-pwm_b.start(0)   # Descomentar esta linea para inicializar el motor en 0% de esfuerzo
+pwm_a.start(0)   
+pwm_b.start(0)  
+os.system('clear')
 
+# Start the motor running
+GPIO.output(in1,False)
+GPIO.output(in2,True)
+GPIO.output(in3,False)
+GPIO.output(in4,True)
+pwm_a.ChangeDutyCycle(100)
+pwm_b.ChangeDutyCycle(100)
+
+# Wait five seconds
+try:
+    time.sleep(1)
+
+# In case the user presses CTRL+C
+except KeyboardInterrupt:
+        print("User finished the program")
+
+# If we get here, either five seconds has passed, or the user pressed CTRL+C.  Either way, stop the motor and clean up
 pwm_a.stop()
 pwm_b.stop()
-
 GPIO.cleanup()
-
 os.system('clear')
-exit()
 
+exit()
